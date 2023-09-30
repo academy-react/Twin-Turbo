c7aa643ab1c4ef4cce13958f49c0678349fb588e
Hamid <stormy@gmail.com>
2023-09-30 22:30:36 +0330

    structure

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './app/App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
