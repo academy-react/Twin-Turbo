6d4c0b3d3174628b993cbeb555fd4724712424e1
mamad <mmdbazooka@gmail.com>
2023-10-01 00:04:36 +0330

    new changes

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,8 +1,22 @@
-const App = () =>  {
+import { createBrowserRouter , RouterProvider } from 'react-router-dom'
+import Header from '../components/Header.jsx'
+import Footer from '../components/Footer.jsx'
 
+const router = createBrowserRouter([
+    {
+        path : '/header',
+        element : <Header />
+    },
+    {
+        path : '/footer',
+        element : <Footer />
+    }
+])
+
+const App = () =>  {
  
     return (
-        <div></div>
+        <RouterProvider router={router} />
     )
 
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './app/App.jsx'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
+import App from './app/App.jsx'
 import './index.css'
-import { createBrowserRouter , RouterProvider } from 'react-router-dom'
-
-const router = createBrowserRouter([
-    {
-      path : '/',
-      element : <Header />
-    }
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <RouterProvider router={router} />
+    <App />
 )
