b76b3f530e697fc4c84c90eb9c5b7c84640e058d
Hamid <stormy@gmail.com>
2023-10-01 11:19:47 +0330

    select

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,8 @@ const Header = () => {
         <div className="my-0 mx-auto w-[1279px] h-full border flex justify-between">
                 <div className="h-full flex justify-around items-center pl-1 text-[#8043bd] [&>div]:h-[36px]  [&>div]:bg-[#f1ebf8] [&>div]:mx-[8px] [&>div]:rounded-[30px] [&>div]:flex [&>div]:justify-center [&>div]:items-center [&>div]:whitespace-nowrap [&>div]:text-x-[14px] [&>div]:cursor-pointer [&>div]:shadow-[0px_3px_6px_#5757574f]">
                 <div className="w-10"><img src="../src/assets/images/shop.png" alt="" className="w-[25px] h-[25px]" /></div>
-                <div className="mx-[15px] w-20 px-4 " >ورود</div>
-                <div className="mx-[15px] w-20 px-4 " >ثبت نام</div>
+                <div className="mx-[15px] w-20 px-4 select-none" >ورود</div>
+                <div className="mx-[15px] w-20 px-4 select-none" >ثبت نام</div>
                 <div className="w-10"><img src="../src/assets/images/search.png" alt="" className="w-[25px] h-[25px]" /></div>
             </div>
             <div className="w-[35%] h-full flex flex-row-reverse justify-around items-center [&>a]:text-[#8043bd] [&>a]:pb-2 ">
