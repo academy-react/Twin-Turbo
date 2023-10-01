01cc5ed57facc50e039b9463840ec94b9e6dbdd9
mamad <mmdbazooka@gmail.com>
2023-10-01 19:28:59 +0330

    complete half of landing

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div className='w-[full] h-[700px]  flex justify-center items-center pt-[100px] relative'>
-        <div className='w-[550px] h-[270px] flex flex-wrap'>
-            <img src="../src/assets/images/blue.png" alt="" className='w-[50%] h-[50%] '/>
-            <img src="../src/assets/images/purple.png" alt="" className='w-[50%] h-[50%] '/>
-            <img src="../src/assets/images/yellow.png" alt="" className='w-[50%] h-[50%] '/>
-            <img src="../src/assets/images/red.png" alt="" className='w-[50%] h-[50%] '/>
+    <div className='w-[full] h-[700px] flex justify-center items-center pt-[100px] relative'>
+        <div className='w-[550px] h-[270px] flex flex-wrap justify-between'>
+            <img src="../src/assets/images/blue.png" alt="" className='w-[48%] h-[48%] mb-5'/>
+            <img src="../src/assets/images/purple.png" alt="" className='w-[48%] h-[48%] '/>
+            <img src="../src/assets/images/yellow.png" alt="" className='w-[48%] h-[48%] '/>
+            <img src="../src/assets/images/red.png" alt="" className='w-[48%] h-[48%] '/>
         </div>
         <img src="../src/assets/images/Group 159.png" alt="" className='absolute left-[-110px] bottom-[-70px] scale-[50%] drop-shadow-[0px_3px_6px_#5757574f]' />
         <img src="../src/assets/images/Path 620.png" alt="" className='absolute left-[-250px] top-[-20px] scale-[50%] drop-shadow-[0px_3px_6px_#5757574f]'/>
