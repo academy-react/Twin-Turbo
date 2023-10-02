05efe98ba593ce1faad916d0108e4ad860585d7c
mamad <mmdbazooka@gmail.com>
2023-10-02 19:16:11 +0330

    last-changes-landing

--- src/components/Recommands.jsx
+++ src/components/Recommands.jsx
@@ -8,15 +8,15 @@ const Recommands = () => {
             <img src="../src/assets/images/re-i.png" className="h-full" />
             <div className="w-[450px] h-[490px] bg-white absolute right-24 top-0 rounded-3xl flex flex-col items-center justify-evenly">
                 <div className="relative w-full flex justify-center">
-                    <input type="text" className="placeholder:text-[#5a0ba951] text-[#5A0BA9] border border-[#70707070] outline-none rounded-lg w-[85%] h-12 pl-5" placeholder="name"/>
+                    <input type="text" className="placeholder:text-[#5a0ba951] text-[#5A0BA9] border border-[#70707023] outline-none rounded-lg w-[85%] h-12 pl-5" placeholder="name"/>
                     <div className="text-[18px] text-[#67008F] absolute right-12 top-3">نام کامل</div>
                 </div>
                 <div className="relative w-full flex justify-center">
-                    <input type="text" className="placeholder:text-[#5a0ba951] text-[#5A0BA9] border border-[#70707070] outline-none rounded-lg w-[85%] h-12 pl-5" placeholder="enter your email address"/>
+                    <input type="text" className="placeholder:text-[#5a0ba951] text-[#5A0BA9] border border-[#70707023] outline-none rounded-lg w-[85%] h-12 pl-5" placeholder="enter your email address"/>
                     <div className="text-[18px] text-[#67008F] absolute right-12 top-3">ایمیل</div>
                 </div>
                 <div className="relative w-full flex justify-center">
-                    <textarea type="text" className="placeholder:text-[#5a0ba951] text-[#5A0BA9] border border-[#70707070] outline-none rounded-lg w-[85%] h-80 pl-5 pt-10 resize-none"/>
+                    <textarea type="text" className="placeholder:text-[#5a0ba951] text-[#5A0BA9] border border-[#70707023] outline-none rounded-lg w-[85%] h-80 pl-5 pt-10 resize-none"/>
                     <div className="text-[18px] text-[#67008F] absolute right-12 top-3">متن پیام</div>
                 </div>
             </div>
