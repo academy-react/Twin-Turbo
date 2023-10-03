448c237a9f42945c56b3da27376d926d01d8d110
Hamid <stormy@gmail.com>
2023-10-03 10:41:42 +0330

    orbit one

--- src/screens/Page404.jsx
+++ src/screens/Page404.jsx
@@ -5,8 +5,11 @@ const Page404 = () => {
   return (
     <div className=' w-[1280px] mx-[auto]'>
         <div className='w-full h-[400px] flex flex-col items-center justify-center'>
-            <img src="../src/assets/images/404.png" alt="" className='scale-[60%]'/>
-            <Link className="w-[20%] h-[45px] rounded-[15px] bg-[#A644C1] flex justify-center items-center text-[#fff] cursor-pointer">بازگشت به صفحه اصلی</Link>
+          <div className='relative w-[450px] h-[400px] flex justify-center items-center'>
+            <img src="../src/assets/images/404.png" alt="" />
+            <img src="../src/assets/images/sircle.png" alt="" className='scale-[70%] absolute right-[45px] top-[140px]' id='sircle1' />
+          </div>
+            <Link className="w-[20%] h-[45px] rounded-[15px] bg-[#A644C1] flex justify-center items-center text-[#fff] cursor-pointer hover:bg-[#8e2aae] transition-all duration-300">بازگشت به صفحه اصلی</Link>
         </div>
         <Footer />
     </div>
