c02fb4b17d477df5f9eee878fa65a25e9e8f4e17
mamad <mmdbazooka@gmail.com>
2023-10-03 15:51:56 +0330

    forget change

--- src/screens/Forget.jsx
+++ src/screens/Forget.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const forget = () => {
   return (
     <div className='w-full bg-gradient-to-r from-[#5A0BA9] to-[#C003B2]'>
-        <div className='w-full mx-auto bg-[url(../src/assets/images/forget-bg.png)] bg-fixed h-[100vh] bg-cover relative scale-x-110'>
-            
+        <div className='w-full mx-auto bg-[url(../src/assets/images/forget-bg.png)] bg-fixed h-[100vh] bg-cover relative scale-x-110 scale-y-[90%]'>
+
         </div>
     </div>
   )
