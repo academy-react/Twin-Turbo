4c7436bb09e709aedd6d5c4f454246ce1046e3d7
Hamid <stormy@gmail.com>
2023-10-03 13:18:40 +0330

    sircle editation

--- src/screens/Page404.jsx
+++ src/screens/Page404.jsx
@@ -8,6 +8,7 @@ const Page404 = () => {
           <div className='relative w-[450px] h-[400px] flex justify-center items-center'>
             <img src="../src/assets/images/404.png" alt="" />
             <img src="../src/assets/images/sircle.png" alt="" className='scale-[70%] absolute right-[45px] top-[140px]' id='sircle1' />
+            <img src="../src/assets/images/sircle.png" alt="" className='scale-[70%] absolute left-[45px] top-[105px]' id='sircle2' />
           </div>
             <Link className="w-[20%] h-[45px] rounded-[15px] bg-[#A644C1] flex justify-center items-center text-[#fff] cursor-pointer hover:bg-[#8e2aae] transition-all duration-300">بازگشت به صفحه اصلی</Link>
         </div>
