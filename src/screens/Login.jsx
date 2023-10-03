3107b76106ca3c2c4009ea717404f5381fc00bf2
mamad <mmdbazooka@gmail.com>
2023-10-03 20:51:37 +0330

    complete login

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const login = () => {
   return (
     <div className='w-full h-[100vh] bg-gradient-to-r from-[#66008C] to-[#9F0099]'>
-      <div className='w-[1280px] border mx-auto h-full relative overflow-hidden '>
+      <div className='w-[1280px] mx-auto h-full relative overflow-hidden '>
           <Header className="hidden" src="avatar-1.png" color="#fff" />
           <img src="../src/assets/images/path forget.png" alt="" className='absolute left-[-31px] top-0 scale-x-[122%] scale-y-[127%]'/>
           <img src="../src/assets/images/login.png" alt="" className='absolute left-[-210px] top-[140px] scale-[65%] '/>
