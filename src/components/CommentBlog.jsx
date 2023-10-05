d881872f81b9f09c772d7192c8c1d815cd3bff1d
Hamid <stormy@gmail.com>
2023-10-05 15:36:42 +0330

    new

--- src/components/CommentBlog.jsx
+++ src/components/CommentBlog.jsx
@@ -1,8 +1,8 @@
 const CommentBlog = () => {
   return (
     <>
-      <div className="w-full h-[650px] bg-[#F5F5F5] rounded-[25px] overflow-hidden relative z-10">
-        <p dir="rtl" className="h-[580px] flex justify-center items-center p-[25px] leading-[28px] ">
+      <div className="w-full h-[650px] bg-[#F5F5F5] rounded-[25px] overflow-hidden relative z-10 py-[25px]">
+        <p dir="rtl" className="h-[580px]  p-[25px] leading-[28px] ">
           نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ سمت
           سرورِ متن‌باز و چندسکویی برای زبان جاوااسکریپت است.نود جی‌اس یا نود
           دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ سمت سرورِ متن‌باز و
--- src/screens/Blog.jsx
+++ src/screens/Blog.jsx
@@ -3,6 +3,7 @@ import Footer from '../components/common/Footer'
 import DescriptionBlog from '../components/DescriptionBlog'
 import CommentBlog from '../components/CommentBlog'
 import { useState } from 'react'
+import { useRef } from 'react'
 
 const Blog = () => {
     const [flag, setFlag] = useState(1)
@@ -22,9 +23,21 @@ const Blog = () => {
         {src:"vs.png",name:"نود جی اس",content:"نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ "},
     ])
 
-    const change = (num,btn) => {
+
+
+    const change = (num) => {
         setFlag(num)
+
+        if(num == 1) {
+            btn2.style.background = "transparent";
+            btn1.style.background = "#F5F5F5";
+        }
+        else if(num == 2) {
+            btn1.style.background = "transparent";
+            btn2.style.background = "#F5F5F5";
+        }
     }
+
     return (
         <>
             <div className='w-[1920px] mx-[auto]'>
@@ -57,8 +70,8 @@ const Blog = () => {
                         <div className='w-full h-[530px] flex justify-center items-center'><img src="../src/assets/images/node.png" alt="" className='scale-x-[110%]'/></div>
                         <div className='w-full  flex justify-center items-center text-[40px]'>نود جی اس</div>
                         <div dir='rtl' className='w-full h-[600px] px-[20px]'>
-                            <button className='p-[10px] border borde-[green] relative right-[35px] top-[8px] rounded-xl ' onClick={(e)=> change(1,e.target)}>نظرات</button>
-                            <button className='p-[10px] border borde-[green] relative right-[65px] top-[8px] rounded-xl ' onClick={(e)=> change(2,e.target)}>توضیحات</button>
+                            <button className='p-[10px] border border-[#eee] borde-[green] relative right-[35px] top-[8px] rounded-xl ' onClick={(e)=> change(1)} id='btn1'>نظرات</button>
+                            <button className='p-[10px] border border-[#eee] borde-[green] relative right-[65px] top-[8px] rounded-xl ' onClick={(e)=> change(2)} id='btn2'>توضیحات</button>
                             {flag == 1 && <DescriptionBlog/>}
                             {flag == 2 && <CommentBlog />}
                         </div>
