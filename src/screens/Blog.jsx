9b1dfcacfc552955c429ff70c0821b76f37c8ca0
Hamid <stormy@gmail.com>
2023-10-06 19:45:05 +0330

    new

--- src/screens/Blog.jsx
+++ src/screens/Blog.jsx
@@ -3,10 +3,9 @@ import Footer from '../components/common/Footer'
 import DescriptionBlog from '../components/DescriptionBlog'
 import CommentBlog from '../components/CommentBlog'
 import { useState } from 'react'
-import { useRef } from 'react'
 
 const Blog = () => {
-    const [flag, setFlag] = useState(1)
+    const [flag, setFlag] = useState(true)
 
     const [item, setItem] = useState([
         {src:"node.png",name:"نود جی اس",content:"نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ "},
@@ -28,13 +27,13 @@ const Blog = () => {
     const change = (num) => {
         setFlag(num)
 
-        if(num == 1) {
+        if(num == true) {
             btn2.style.background = "transparent";
-            btn1.style.background = "#F5F5F5";
+            btn1.style.background = "#f1ebf8";
         }
-        else if(num == 2) {
+        else if(num == false) {
             btn1.style.background = "transparent";
-            btn2.style.background = "#F5F5F5";
+            btn2.style.background = "#f1ebf8";
         }
     }
 
@@ -70,10 +69,10 @@ const Blog = () => {
                         <div className='w-full h-[530px] flex justify-center items-center'><img src="../src/assets/images/node.png" alt="" className='scale-x-[110%]'/></div>
                         <div className='w-full  flex justify-center items-center text-[40px]'>نود جی اس</div>
                         <div dir='rtl' className='w-full h-[600px] px-[20px]'>
-                            <button className='p-[10px] border border-[#eee] borde-[green] relative right-[35px] top-[8px] rounded-xl ' onClick={(e)=> change(1)} id='btn1'>نظرات</button>
-                            <button className='p-[10px] border border-[#eee] borde-[green] relative right-[65px] top-[8px] rounded-xl ' onClick={(e)=> change(2)} id='btn2'>توضیحات</button>
-                            {flag == 1 && <DescriptionBlog/>}
-                            {flag == 2 && <CommentBlog />}
+                            <button className='p-[10px] border border-[#dddd] relative right-[35px] top-[8px] rounded-xl ' onClick={(e)=> change(true)} id='btn1' style={{background:"#f1ebf8"}}>نظرات</button>
+                            <button className='p-[10px] border border-[#dddd] relative right-[65px] top-[8px] rounded-xl ' onClick={(e)=> change(false)} id='btn2'>توضیحات</button>
+                            {flag == true && <DescriptionBlog/>}
+                            {flag == false && <CommentBlog />}
                         </div>
                     </div>
 
--- src/screens/Landing.jsx
+++ src/screens/Landing.jsx
@@ -6,27 +6,37 @@ import Masters from "../components/Masters";
 import News from "../components/News";
 import TopLanding from "../components/TopLanding";
 import Recommands from "../components/Recommands";
+import { useLocation } from "react-router-dom";
 
 const Landing = () => {
-  // window.onscroll = ()=> {
-  //   console.log(pageYOffset);
-  //   if(pageYOffset > 140) man.style.right = "-140px"
-  //   if(pageYOffset > 240) holder.style.transform = "scale(100%)"
-  //   if(pageYOffset > 300) woman.style.left = "-110px"
-  //   if(pageYOffset > 710) planet1.style.left = "-40px"
-  //   if(pageYOffset > 1100) planet2.style.right = "40px"
-  //   if(pageYOffset > 1700) coursesHolder2.style.right = "145px"
-  //   if(pageYOffset > 1900) coursesHolder1.style.left = "100px"
-  //   if(pageYOffset > 1900) coursesHolder3.style.left = "160px"
-  //   if(pageYOffset > 3000) {
-  //     holderMasters.style.width = "100%"
-  //     holderMasters.style.opacity = "1"
-  //   }
-  //   if(pageYOffset > 3800) {
-  //     recommandsForm.style.height = "600px"
-  //     recommandsForm.style.opacity = "1"
-  //   }
-  // }
+
+
+
+  window.onscroll = ()=> {
+    
+
+
+
+    if(window.location.pathname == "/") {
+      if(pageYOffset > 140) man.style.right = "-140px"
+      if(pageYOffset > 240) holder.style.transform = "scale(100%)"
+      if(pageYOffset > 300) woman.style.left = "-110px"
+      if(pageYOffset > 710) planet1.style.left = "-40px"
+      if(pageYOffset > 1100) planet2.style.right = "40px"
+      if(pageYOffset > 1700) coursesHolder2.style.right = "145px"
+      if(pageYOffset > 1900) coursesHolder1.style.left = "100px"
+      if(pageYOffset > 1900) coursesHolder3.style.left = "160px"
+      if(pageYOffset > 3000) {
+        holderMasters.style.width = "100%"
+        holderMasters.style.opacity = "1"
+      }
+      if(pageYOffset > 3800) {
+        recommandsForm.style.height = "600px"
+        recommandsForm.style.opacity = "1"
+      }
+    }
+
+  }
 
   return (
     <div className="w-[1920px] mx-auto my-0 overflow-hidden">
