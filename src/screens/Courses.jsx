9dd627b227aa5803f24cfbc47d79d9bd2a3e5812
mamad <mmdbazooka@gmail.com>
2023-10-07 19:55:43 +0330

    move files

--- src/screens/Courses.jsx
+++ src/screens/Courses.jsx
@@ -1,48 +1,41 @@
-import React, { useState } from 'react'
-import Header from '../components/common/Header'
-import Footer from '../components/common/Footer'
-import DescriptionBlog from '../components/DescriptionBlog'
-import CommentBlog from '../components/CommentBlog'
-import RightPanel from '../components/RightPanel'
+import React, { useState } from "react";
+import Header from "../components/common/Header";
+import Footer from "../components/common/Footer";
+import DescriptionBlog from "../components/selectedBlog/DescriptionBlog";
+import CommentBlog from "../components/selectedBlog/CommentBlog";
+import RightPanel from "../components/common/RightPanel";
 
 const Courses = () => {
-    const [flag, setFlag] = useState(1)
-    const change = (num) => {
-        setFlag(num)
+  const [flag, setFlag] = useState(1);
+  const change = (num) => {
+    setFlag(num);
 
-        if(num == 1) {
-            btn2.style.background = "transparent";
-            btn1.style.background = "#F5F5F5";
-        }
-        else if(num == 2) {
-            btn1.style.background = "transparent";
-            btn2.style.background = "#F5F5F5";
-        }
+    if (num == 1) {
+      btn2.style.background = "transparent";
+      btn1.style.background = "#F5F5F5";
+    } else if (num == 2) {
+      btn1.style.background = "transparent";
+      btn2.style.background = "#F5F5F5";
     }
+  };
 
-    return (
-        <>
-            <div className='w-[1920px] mx-[auto]'>
+  return (
+    <>
+      <div className="w-[1920px] mx-[auto]">
+        <Header src="avatar.png" color="#5A0BA9" />
 
-                <Header  src="avatar.png" color="#5A0BA9" />
+        <div className="w-[full] h-[1400px] flex justify-center items-center gap-[50px]">
+          <div
+            dir="ltr"
+            className="w-[30%] h-[1300px] flex flex-col items-center justify-center rounded-xl shadow-[0_0_7px_#ddd] bg-white [&>div]:w-[85%] [&>div]:h-[220px] [&>div]:rounded-2xl [&>div]:shadow-[0_0_7px_#ccc] [&>div]:flex [&>div]:flex-col [&>div]:my-[10px]"
+          ></div>
+          <RightPanel src="angularCourses.png" name="انگولار" />
+        </div>
 
-                <div className="w-[full] h-[1400px] flex justify-center items-center gap-[50px]">
+        <Footer />
+      </div>
+    </>
+  );
+};
 
-                    <div dir='ltr' className="w-[30%] h-[1300px] flex flex-col items-center justify-center rounded-xl shadow-[0_0_7px_#ddd] bg-white [&>div]:w-[85%] [&>div]:h-[220px] [&>div]:rounded-2xl [&>div]:shadow-[0_0_7px_#ccc] [&>div]:flex [&>div]:flex-col [&>div]:my-[10px]">
-                        
-                    </div>
-                    <RightPanel src="angularCourses.png" name="انگولار"/>
-                </div>
-
-                <Footer />
-
-
-
-            </div>
-
-        </>
-        
-    )
-}
-
-export default Courses
+export default Courses;
--- src/screens/SelectedBlog.jsx
+++ src/screens/SelectedBlog.jsx
@@ -1,71 +1,129 @@
-import Header from '../components/common/Header'
-import Footer from '../components/common/Footer'
-import RightPanel from '../components/RightPanel'
-import { useState } from 'react'
+import Header from "../components/common/Header";
+import Footer from "../components/common/Footer";
+import RightPanel from "../components/common/RightPanel";
+import { useState } from "react";
 
 const Blog = () => {
+  const [item, setItem] = useState([
+    {
+      src: "node.png",
+      name: "نود جی اس",
+      content:
+        "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+    },
+    {
+      src: "nest.png",
+      name: "نود جی اس",
+      content:
+        "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+    },
+    {
+      src: "vs.png",
+      name: "نود جی اس",
+      content:
+        "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+    },
+    {
+      src: "node.png",
+      name: "نود جی اس",
+      content:
+        "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+    },
+    {
+      src: "nest.png",
+      name: "نود جی اس",
+      content:
+        "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+    },
+    {
+      src: "vs.png",
+      name: "نود جی اس",
+      content:
+        "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+    },
+    {
+      src: "node.png",
+      name: "نود جی اس",
+      content:
+        "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+    },
+    {
+      src: "nest.png",
+      name: "نود جی اس",
+      content:
+        "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+    },
+    {
+      src: "vs.png",
+      name: "نود جی اس",
+      content:
+        "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+    },
+    {
+      src: "node.png",
+      name: "نود جی اس",
+      content:
+        "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+    },
+    {
+      src: "nest.png",
+      name: "نود جی اس",
+      content:
+        "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+    },
+    {
+      src: "vs.png",
+      name: "نود جی اس",
+      content:
+        "نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ ",
+    },
+  ]);
 
-    const [item, setItem] = useState([
-        {src:"node.png",name:"نود جی اس",content:"نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ "},
-        {src:"nest.png",name:"نود جی اس",content:"نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ "},
-        {src:"vs.png",name:"نود جی اس",content:"نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ "},
-        {src:"node.png",name:"نود جی اس",content:"نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ "},
-        {src:"nest.png",name:"نود جی اس",content:"نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ "},
-        {src:"vs.png",name:"نود جی اس",content:"نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ "},
-        {src:"node.png",name:"نود جی اس",content:"نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ "},
-        {src:"nest.png",name:"نود جی اس",content:"نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ "},
-        {src:"vs.png",name:"نود جی اس",content:"نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ "},
-        {src:"node.png",name:"نود جی اس",content:"نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ "},
-        {src:"nest.png",name:"نود جی اس",content:"نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ "},
-        {src:"vs.png",name:"نود جی اس",content:"نود جی‌اس یا نود دات جی‌اس (به انگلیسی: Node.js) یک محیط اجراییِ "},
-    ])
+  return (
+    <>
+      <div className="w-[1920px] mx-[auto]">
+        <Header src="avatar.png" color="#5A0BA9" />
 
-
-
-    
-
-    return (
-        <>
-            <div className='w-[1920px] mx-[auto]'>
-
-                <Header  src="avatar.png" color="#5A0BA9" />
-
-                <div className="w-[full] h-[1400px] flex justify-center items-center gap-[50px]">
-
-                    <div dir='ltr' className="w-[30%] h-[1300px] rounded-xl shadow-[0_0_7px_#ddd] bg-white overflow-y-scroll">
-                        <div dir='rtl' className='w-[28%] h-[52px] rounded-[18px] bg-white shadow-[0_0_7px_#ddd] flex justify-around items-center [&>img]:w-[30px] [&>p]:text-[20px] my-[20px] mx-[25px] relative left-[350px]'>
-                            <img src="../src/assets/images/menuu.png" alt="" />
-                            <p>بلاگ ها</p>
-                        </div>
-                        <div dir='rtl' className='w-full'>
-                            {item.map((element,index)=> {
-                                return (
-                                    <div key={index} className='w-full h-[120px] flex'>
-                                        <div className='w-[30%] h-full flex items-center justify-center'><img src={"../src/assets/images/" + element.src} alt="" className='h-[110px] rounded-[15px]' /></div>
-                                        <div className='w-[70%] h-full py-[5px] px-[15px] flex flex-col justify-center'>
-                                            <p className='text-[25px] '>{element.name}</p>
-                                            <p className='text-[#777]'>{element.content}</p>
-                                        </div>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                    </div>
-
-                    <RightPanel src="Node.png" name="نود جی اس"/>
-
-
-                </div>
-
-                <Footer />
-
-
-
+        <div className="w-[full] h-[1400px] flex justify-center items-center gap-[50px]">
+          <div
+            dir="ltr"
+            className="w-[30%] h-[1300px] rounded-xl shadow-[0_0_7px_#ddd] bg-white overflow-y-scroll"
+          >
+            <div
+              dir="rtl"
+              className="w-[28%] h-[52px] rounded-[18px] bg-white shadow-[0_0_7px_#ddd] flex justify-around items-center [&>img]:w-[30px] [&>p]:text-[20px] my-[20px] mx-[25px] relative left-[350px]"
+            >
+              <img src="../src/assets/images/menuu.png" alt="" />
+              <p>بلاگ ها</p>
             </div>
+            <div dir="rtl" className="w-full">
+              {item.map((element, index) => {
+                return (
+                  <div key={index} className="w-full h-[120px] flex">
+                    <div className="w-[30%] h-full flex items-center justify-center">
+                      <img
+                        src={"../src/assets/images/" + element.src}
+                        alt=""
+                        className="h-[110px] rounded-[15px]"
+                      />
+                    </div>
+                    <div className="w-[70%] h-full py-[5px] px-[15px] flex flex-col justify-center">
+                      <p className="text-[25px] ">{element.name}</p>
+                      <p className="text-[#777]">{element.content}</p>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
 
-        </>
-        
-    )
-}
+          <RightPanel src="Node.png" name="نود جی اس" />
+        </div>
 
-export default Blog
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Blog;
