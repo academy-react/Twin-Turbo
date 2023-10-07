263ff3ced4c3d0085bb5fec510da112fa045a804
Hamid <stormy@gmail.com>
2023-10-07 18:54:46 +0330

    new

--- src/components/landing/Navigation.jsx
+++ src/components/landing/Navigation.jsx
@@ -1,6 +1,6 @@
 const Navigation = () => {
     return (
-      <div className="w-[60px] h-[60px] brightness-[120%] rounded-full fixed right-2 bottom-2 flex justify-center items-center cursor-pointer transition-all duration-[.5s]" style={{opacity:0}} id="nav" onClick={()=> document.documentElement.scrollTop = 0}>
+      <div className="w-[60px] h-[60px] brightness-[120%] rounded-full fixed right-2 bottom-2 flex justify-center items-center cursor-pointer transition-all duration-[.5s] z-10" style={{opacity:0}} id="nav" onClick={()=> document.documentElement.scrollTop = 0}>
           <img src="../src/assets/images/navigation.png" alt="" className="absolute left-0 top-0" />
           <img src="../src/assets/images/up.png" alt="" className="relative z-10 scale-[50%] bottom-[6px]"/>
       </div>
