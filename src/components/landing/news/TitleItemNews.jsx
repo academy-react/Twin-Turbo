96505e44ee5664c1a3bf6e0ca86fc12d49bdbf87
Hamid <stormy@gmail.com>
2023-10-14 22:12:20 +0330

    new

---
+++ src/components/landing/news/TitleItemNews.jsx
@@ -0,0 +1,9 @@
+const TitleItemNews = ({content}) => {
+  return (
+    <div className="max-[460px]:hidden absolute top-[-25px] right-[-15px] p-2 w-16 text-center shadow-[0_0_7px_#ccc] bg-white rounded-[15px] ">
+        {content}
+    </div>
+  )
+}
+
+export default TitleItemNews
