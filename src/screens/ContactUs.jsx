9f3483fe2d8a5b9730e6b20354e895698c407178
Hamid <stormy@gmail.com>
2023-10-15 23:40:12 +0330

    new

--- src/screens/ContactUs.jsx
+++ src/screens/ContactUs.jsx
@@ -1,34 +1,42 @@
 import {Header,Footer} from '../components/common'
-
+import {Form,Formik,Field,ErrorMessage} from 'formik'
 
 const ContactUs = () => {
+
+    const log = (value)=> {
+        console.log(value);
+    }
 
     return (
         <div className=' w-[1920px] mx-[auto]'>
             <Header src="avatar.png" color="#5A0BA9" />
                 <div className='w-[80%] h-[800px] mx-auto flex items-center justify-around my-10'>
-                    <div className="w-[600px] h-[750px] shadow-[0_0_7px_#ddd] bg-white rounded-3xl flex flex-col items-center justify-evenly transition-all duration-1000" id="recommandsForm">
-                        <div className="text-[30px]">تماس با ما</div>
-                        <div className="relative w-[85%] flex justify-between items-center border rounded-lg pr-[15px] overflow-hidden">
-                            <input type="text" className="text-[18px] w-[70%] placeholder:text-[#5a0ba951] text-[#5A0BA9]  border-[#70707023] outline-none rounded-lg h-12 pl-5" placeholder="name"/>
-                            <div className="text-[20px] text-[#67008F] transiition-all duration-[.5s]">نام و نام خانوادگی</div>
-                        </div>
-                        <div className="relative w-[85%] flex justify-between items-center border rounded-lg pr-[15px] overflow-hidden">
-                            <input type="text" className="text-[18px] w-[75%]  placeholder:text-[#5a0ba951] text-[#5A0BA9]  border-[#70707023] outline-none  h-12 pl-5" placeholder="enter your email address"/>
-                            <div className="text-[20px] text-[#67008F] transiition-all duration-[.5s]">ایمیل</div>
-                        </div>
-                        <div className="relative w-[85%] flex justify-between items-center border rounded-lg pr-[15px] overflow-hidden">
-                            <input type="text" className="text-[18px] w-[75%]  placeholder:text-[#5a0ba951] text-[#5A0BA9]  border-[#70707023] outline-none  h-12 pl-5" placeholder="phone call"/>
-                            <div className="text-[20px] text-[#67008F] transiition-all duration-[.5s]">شماره موبایل</div>
-                        </div>
-                        <div className="relative w-full flex justify-center">
-                            <textarea type="text" className="placeholder:text-[#5a0ba951] text-[#5A0BA9] border border-[#70707023] outline-none rounded-lg w-[85%] h-60 pl-5 pt-10 px-[5px] resize-none"/>
-                            <div className="text-[20px] text-[#67008F] absolute right-[58px] top-3 transiition-all duration-[.5s]">متن پیام</div>
-                        </div>
-                        <button className='w-[89%] h-16 flex justify-center items-center bg-[#36C54E] text-white rounded-2xl text-[22px] hover:bg-[#38b24c] transition-all duration-300'>
-                            ارسال پیام
-                        </button>
-                    </div>
+                    
+
+                    <Formik initialValues={{name:"",email:"",phone:"",textarea:""}} onSubmit={(values)=> log(values)}>
+                        <Form className="w-[600px] h-[750px] shadow-[0_0_7px_#ddd] bg-white rounded-3xl flex flex-col items-center justify-evenly transition-all duration-1000" id="recommandsForm">
+                            <div className="text-[30px]">تماس با ما</div>
+                            <div className="relative w-[85%] flex justify-between items-center border rounded-lg pr-[15px] overflow-hidden">
+                                <Field name="name" type="text" className="text-[18px] w-[70%] placeholder:text-[#5a0ba951] text-[#5A0BA9]  border-[#70707023] outline-none rounded-lg h-12 pl-5" placeholder="name"/>
+                                <div className="text-[20px] text-[#67008F] transiition-all duration-[.5s]">نام و نام خانوادگی</div>
+                            </div>
+                            <div className="relative w-[85%] flex justify-between items-center border rounded-lg pr-[15px] overflow-hidden">
+                                <Field name="email" type="text" className="text-[18px] w-[75%]  placeholder:text-[#5a0ba951] text-[#5A0BA9]  border-[#70707023] outline-none  h-12 pl-5" placeholder="enter your email address"/>
+                                <div className="text-[20px] text-[#67008F] transiition-all duration-[.5s]">ایمیل</div>
+                            </div>
+                            <div className="relative w-[85%] flex justify-between items-center border rounded-lg pr-[15px] overflow-hidden">
+                                <Field name="phone" type="text" className="text-[18px] w-[75%]  placeholder:text-[#5a0ba951] text-[#5A0BA9]  border-[#70707023] outline-none  h-12 pl-5" placeholder="phone call"/>
+                                <div className="text-[20px] text-[#67008F] transiition-all duration-[.5s]">شماره موبایل</div>
+                            </div>
+                            <div className="relative w-full flex justify-center">
+                                <Field type="textarea" name="textarea" className="placeholder:text-[#5a0ba951] text-[#5A0BA9] border border-[#70707023] outline-none rounded-lg w-[85%] h-60 pl-5 pt-10 px-[5px] resize-none"/>
+                                <div className="text-[20px] text-[#67008F] absolute right-[58px] top-3 transiition-all duration-[.5s]">متن پیام</div>
+                            </div>
+                            <button type='submit' className='w-[89%] h-16 flex justify-center items-center bg-[#36C54E] text-white rounded-2xl text-[22px] hover:bg-[#38b24c] transition-all duration-300'>
+                                ارسال پیام
+                            </button>
+                        </Form>
+                    </Formik>
                         
                     <ul className="w-[450px] h-[500px] flex flex-col justify-around items-end self-start">
                         <li className="flex items-center ">
