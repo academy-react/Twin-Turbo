d13f96bbe40792918c67ceec0df48106d38b0d58
mamad <mmdbazooka@gmail.com>
2023-10-29 21:25:01 +0330

    make account dashboard and editprofile

---
+++ src/core/services/account.js
@@ -0,0 +1,12 @@
+let account = {
+    image : "../src/assets/images/dashboard/khoros.jpg" ,
+    username : "Mmd && Hamid" , 
+    role : "طراح فرانت" , 
+    email : "stormy85@gmail.com" ,
+    birthDayDate : "1385/7/17" , 
+    nationalCode : "2081326948" , 
+    phone : "09909125482" , 
+    status : "فعال"
+}
+
+export default account
