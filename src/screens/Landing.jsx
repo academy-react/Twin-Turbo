6439854b238dd984a3eb3a12d6a128c096887a6a
mamad <mmdbazoo@gmail.com>
2023-11-11 19:57:24 +0330

    technologies api

--- src/screens/Landing.jsx
+++ src/screens/Landing.jsx
@@ -2,29 +2,16 @@ import { useEffect } from "react";
 import Footer from "../components/common/Footer";
 import {Category , Courses , Masters , Navigation , News , Recommands , Services , HeroSection} from '../components/landing'
 import landingScrollLogic from "../core/utils/landingScroll.utils";
-import customAxios from "../core/services/interceptor";
 
 const Landing = () => {
-
-  const getCourse = async ()=> {
-
-      let result =  await customAxios.get("/News/GetListNewsCategor")
-      console.log(result);
-
-  }
   
   useEffect(() => {
-    
-      getCourse()
       document.addEventListener("scroll",()=> landingScrollLogic())
-      
   }, [])
 
   return (
     <div className="w-[1920px] mx-auto my-0 overflow-hidden max-[1919px]:w-full relative">
       <div className="h-[5px] bg-[#9f009a] fixed top-0 left-0 z-[1000] dark:bg-[#fff]" style={{width:"0px"}} id="scrollNav"></div>
-      {/* <DarkBtn /> */}
-      
       <Navigation />
       <HeroSection />
       <Services />
