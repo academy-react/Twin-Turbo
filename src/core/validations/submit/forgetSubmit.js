cd7f98185b7351219e07112ebe7c4273d71d22e7
Hamid <stormy@gmail.com>
2023-11-15 21:17:29 +0330

    forget pass api

--- src/core/validations/submit/forgetSubmit.js
+++ src/core/validations/submit/forgetSubmit.js
@@ -1,5 +1,58 @@
+import { useParams } from "react-router-dom";
+import customAxios from "../../services/interceptor";
+
+const postEmail = async (email,baseUrl) => {
+
+    
+    let res = await customAxios.post("/Sign/ForgetPassword",
+    {
+        email: email,
+        baseUrl: baseUrl
+    })
+    console.log(res);
+    get()
+}
+
+
+const get = async ()=> {
+    
+    let ConfigValue = location.pathname.slice(location.pathname.lastIndexOf("/")+1)
+    let res = await customAxios.get("/Sign/Reset/" + ConfigValue)
+    
+    sessionStorage.setItem("userId",res.id)
+    sessionStorage.setItem("userMessage",res.message)
+
+}
+
+
+const postNewPass = async (values)=> {
+
+    let userId = sessionStorage.getItem("userId")
+    let userMessage = sessionStorage.getItem("userMessage")
+
+    userId = Math.ceil(userId)
+
+    customAxios.post("/Sign/Reset",
+    {
+      userId: userId,
+      newPassword: values.newPassword,
+      resetValue: userMessage
+    })
+
+}
+
+
 const forgetSubmit = (values) => {
-    console.log(values);
+    postEmail(values.email,location.href)
+    if(values.newPassword.length >= 8){
+        alert("success")
+        postNewPass(values)
+        location.pathname = "/login"
+    }
+    else {
+        alert("رمز باید بیشتر از 8 کلمه باشد")
+    }
+
 }
 
 export default forgetSubmit
--- src/screens/ForgetPassword.jsx
+++ src/screens/ForgetPassword.jsx
@@ -2,44 +2,24 @@ import { Formik , Form } from "formik"
 import forgetSubmit from "../core/validations/submit/forgetSubmit";
 import { LinkComponent , FieldInput , Header, Submit } from '../components/common';
 import forgetPasswordValidation from "../core/validations/forgetPasswordValidation";
-import { useParams } from "react-router-dom";
 import { useEffect } from "react";
-import customAxios from "../core/services/interceptor";
+import { useState } from "react";
 
 const ForgetPassword = () => {
-  let url = useParams()
 
 
+    const [flag, setFlag] = useState(1)
+  
 
-  const get = async ()=> {
-      let res = await customAxios.get("/Sign/Reset/" + url.ConfigValue)
-      console.log(res);
-      
-      sessionStorage.setItem("userId",res.id)
-      sessionStorage.setItem("userMessage",res.message)
-
-  }
-
-  const post = async ()=> {
-
-    let userId = sessionStorage.getItem("userId")
-    let userMessage = sessionStorage.getItem("userMessage")
-
-    userId = Math.ceil(userId)
-
-    console.log(userId,userMessage);
-
-    let res = await customAxios.post("/Sign/Reset",
-    {
-      userId: userId,
-      newPassword: "Ssstormy#so123",
-      resetValue: userMessage
-    })
-    console.log(res);
-  }
 
   useEffect(() => {
-      // get()
+
+      if(location.pathname !== "/forgetPassword" && location.pathname !== "/forgetPassword/" ) {
+          setFlag(flag+1)
+          console.log(flag);
+      }
+
+      
   }, [])
   
   return (
@@ -51,7 +31,7 @@ const ForgetPassword = () => {
           <img src="../src/assets/images/panel/path forget.png" alt="" className='absolute left-[-31px] top-0 scale-x-[122%] scale-y-[120%] max-[1000px]:hidden'/>
           <img src="../src/assets/images/panel/forget logo woman.png" alt="" className='absolute left-[0] top-[260px] w-[65%] transition-all duration-1000 max-[1485px]:top-[200px] order-2 max-[1256px]:mt-20 max-[1256px]:right-0 max-[1256px]:top-0 max-[1256px]:relative max-[700px]:hidden '/>
 
-          <Formik initialValues={{email: ""}} onSubmit={(values) => forgetSubmit(values)} validationSchema={forgetPasswordValidation}>
+          <Formik initialValues={{email: "",newPassword:""}} onSubmit={(values) => forgetSubmit(values)} validationSchema={forgetPasswordValidation}>
             
             <Form className="w-[450px] h-[480px] order-1 bg-white absolute right-[16%] top-[340px] transition-all duration-1000 shadow-[0_0_7px_#ccc] rounded-[15px] flex flex-col items-center justify-center max-[550px]:scale-[80%] max-[430px]:scale-[70%] max-[340px]:scale-[65%] max-[1256px]:right-0 max-[1256px]:top-0 max-[1256px]:relative">
               
@@ -59,7 +39,9 @@ const ForgetPassword = () => {
                 <p className='py-[35px] text-[26px]'>فراموشی رمز عبور</p>
                 <div className='w-full h-[160px] flex flex-col items-center justify-center [&>*]:my-[5px]'>
 
-                  <FieldInput name="email" content="" placeholder="ایمیل" dir="rtl" border="border border-[#a361a1]" display="text-[#b9b7b7] text-[15px]" className="rounded-[14px] h-[70px]" />
+                  {flag == 1 && <FieldInput name="email" content="" placeholder="ایمیل" dir="rtl" border="border border-[#a361a1]" display="text-[#b9b7b7] text-[15px]" className="rounded-[14px] h-[70px]" />}
+                  {flag == 2 && <FieldInput name="newPassword" content="" placeholder="رمز جدید" dir="rtl" border="border border-[#a361a1]" display="text-[#b9b7b7] text-[15px]" className="rounded-[14px] h-[70px]" />}
+                  
                   <Submit content="ادامه" className="w-[85%] h-[80px]" />
                 
                 </div>
