0d531045b6a27a5e5ac02c2bee92bf68acba6a2c
mamad <mmdbazoo@gmail.com>
2023-11-22 22:05:21 +0330

    Register Timer

--- src/core/utils/spamTimer.utils.js
+++
@@ -1,8 +0,0 @@
-const interval = (codeS , codeM , setCodeS , setCodeM) => {
-    let timer = setInterval(() => {
-        setCodeS(codeS - 1)
-        if(codeS == 0) setCodeM(codeM - 1)
-        if(codeM == 0 && codeS == 0) clearInterval(timer)
-    }, 1000);
-}
-export default interval
