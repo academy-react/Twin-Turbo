77de3f741c6fe4ccce633e38d928495bfd54fb58
mamad <mmdbazoo@gmail.com>
2023-11-26 19:10:04 +0330

    split router

--- src/app/config/Router.jsx
+++ src/app/config/Router.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import {Landing,ForgetPassword,Login,ContactUs,Blogs,SelectedBlog,Courses,SelectedCourse,Page404, Register , Panel ,ListOfCourse,Mycourse,EditProfile,DashBoard,ChangePassword} from '../../screens'
 
-const Router = createBrowserRouter([
+const Authorize = createBrowserRouter([
     {
       path: "/",
       element: <Landing />,
@@ -92,4 +92,56 @@ const Router = createBrowserRouter([
 
 ]);
 
-export default Router
+const UnAuthorize = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />,
+  },
+  {
+    path: "/forgetPassword/:ConfigValue",
+    element: <ForgetPassword />,
+  },
+  {    // when the forget page was optional the 404 page didn`t worked when we write anything in url
+    path: "/forgetPassword",
+    element: <ForgetPassword />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/contact-us",
+    element: <ContactUs />,
+  },
+  {
+    path: "/blogs",
+    element: <Blogs />,
+  },
+  {
+    path: "/:blogs/:id",
+    element: <SelectedBlog />,
+  },
+  {
+    path: "/courses",
+    element: <Courses />,
+  },
+  {
+    path: "/courses/:id",
+    element: <SelectedCourse />,
+  },
+  {
+    path: "/courses/:id",
+    element: <SelectedCourse />,
+  },
+  {
+    path: "*",
+    element: <Page404 />,
+  },
+
+]);
+
+export { Authorize , UnAuthorize }
