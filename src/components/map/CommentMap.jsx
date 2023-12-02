5023624b68f9741fe355e65662d0eeb14c2882ec
mamad <mmdbazoo@gmail.com>
2023-12-02 18:34:33 +0330

    userIcon

--- src/components/map/CommentMap.jsx
+++ src/components/map/CommentMap.jsx
@@ -82,8 +82,6 @@ const CommentMap = ({ db , parentComment }) => {
                         parentId: element.id,
                     })
                 }
-
-
             }
             idea.appendChild(accBtn);
 
@@ -126,7 +124,7 @@ const CommentMap = ({ db , parentComment }) => {
         db?.map((element,index)=> {
             return (
                 <div key={index} className={`w-full flex items-center gap-[15px] my-[7px] py-5`} data-order={index+5} style={{order:index+5}} >
-                    <img src={element.pictureAddress} alt="" className="w-16 h-[60px] rounded-full " />
+                    <img src={element.pictureAddress ? element.pictureAddress : "../src/assets/images/icons/userIcon.png"} alt="" className="w-16 h-[60px] rounded-full " />
 
                     <div className="w-full h-[100%] bg-white shadow-[0_0_7px_#999] rounded-[15px] p-[10px] relative">
                         <div  className="text-[18px] my-1 flex [&>span]:mx-[10px]">
